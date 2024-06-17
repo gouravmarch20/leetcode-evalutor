440e86e4b5588544defdb071fca69b3d74a8d75e
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2024-06-17 10:25:52 +0530

    jest l

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Express } from "express";
+
+import serverConfig from "./config/serverConfig";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
+import apiRouter from "./routes";
+import SampleWorker from "./workers/SampleWorker";
+
+
+const app: Express = express();
+
+app.use('/api', apiRouter);
+
+app.listen(serverConfig.PORT, () => {
+  console.log(`Server started at *:${serverConfig.PORT}`);
+
+  SampleWorker('SampleQueue');
+
+  sampleQueueProducer('SampleJob', {
+    name: "Sanket",
+    company: "Microsoft",
+    position: "SDE 2 L61",
+    locatiion: "Remote | BLR | Noida"
+  } , 5);
+  sampleQueueProducer('SampleJob', {
+    name: "Gourav",
+    company: "Kogo",
+    position: "SDE 1  ",
+    locatiion: "Delhi"
+  } , 2);
+});
