761b513d856a739ce1e36b67a13d22a9d11addf8
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2024-06-17 23:20:58 +0530

    validitor using zod

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,21 @@
+import bodyParser from "body-parser";
 import express, { Express } from "express";
 
+import bullBoardAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
-import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
-
 
 const app: Express = express();
 
 app.use('/api', apiRouter);
-
+app.use('/ui', bullBoardAdapter.getRouter());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at *:${serverConfig.PORT}`);
-
+  console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
   SampleWorker('SampleQueue');
 
-  sampleQueueProducer('SampleJob', {
-    name: "Sanket",
-    company: "Microsoft",
-    position: "SDE 2 L61",
-    locatiion: "Remote | BLR | Noida"
-  } , 5);
-  sampleQueueProducer('SampleJob', {
-    name: "Gourav",
-    company: "Kogo",
-    position: "SDE 1  ",
-    locatiion: "Delhi"
-  } , 2);
 });
