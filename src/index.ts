d60da2ce63a8841da9330f9f9624a8a8ce67ed33
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2024-06-23 20:56:54 +0530

    docker python setup

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express, { Express } from "express";
 
 import bullBoardAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
+import runPython from "./containers/runPythonDocker";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
-
 const app: Express = express();
 
 app.use('/api', apiRouter);
@@ -17,5 +17,15 @@ app.listen(serverConfig.PORT, () => {
   console.log(`Server started at *:${serverConfig.PORT}`);
   console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
   SampleWorker('SampleQueue');
+  const code = `x = input()
+y = input()
+print("value of x is", x)
+print("value of y is", y)
+`;
 
+  const inputCase = `100
+200
+`;
+
+  runPython(code, inputCase);
 });
